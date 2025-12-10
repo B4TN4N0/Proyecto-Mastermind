bc9d2fb4dd8f621af374f378678a6e5f895060f6
joaquin240606fer-collab <joaquin240606fer@gmail.com>
2025-12-10 13:05:40 +0100

    feat(Añadir nuevos casos test): :sparkles: Añadir caso test de repeticiones
    Añadimos el caso test que comprueba que los genes se pueden repetir en un mismo genoma

--- Tests/Test_mastermind_game.py
+++ Tests/Test_mastermind_game.py
@@ -13,7 +13,7 @@ import pytest
 
 # ESTE TEST COMPROBARÁ QUE LA POBLACIÓN INICIAL TIENE EL TAMAÑO CORRECTO
 
-@pytest.mark.people
+@pytest.mark.population
 
 def test_tamaño_poblacion_inicial(first_population): 
     assert len(first_population)== 100
@@ -23,21 +23,33 @@ def test_tamaño_poblacion_inicial(first_population):
 
 # ESTE TEST COMPROBARÁ QUE CADA INDIVIDUO DE LA POBLACIÓN INICIAL TIENE EL TAMAÑO CORRECTO
 
-@pytest.mark.people
+@pytest.mark.population
 def test_tamaño_genoma_poblacion_inicial(first_population):
     for individual in first_population:
         assert len(individual) == 4
 
 # ------------------------------------------------------------------ #
 
-# Este test comprueba que los colores en la población inicial son válidos
+# Este test comprueba que los colores en la población inicial son válidos #
 
-@pytest.mark.people
+@pytest.mark.population
 def test_colores_validos_poblacion_inicial(first_population,color_genes):
     for gen in first_population:
         for value in gen:
             assert value in color_genes
 
 # ------------------------------------------------------------------ #
+
+# Este test comprueba que puedan existir convinaciones con colores repetidos #
+
+@pytest.mark.population
+def test_genes_repetidos_en_misma_cadena(first_population,color_genes):
+    assert first_population == ['Rojo','Azul','Rosa','Blanco']
+    assert first_population == ['Rojo','Rojo','Azul','Naranja']
+    assert first_population == ['Rojo','Rojo','Rojo','Verde']
+    assert first_population == ['verde','verde','Rojo','Rojo']
+    assert first_population == ['Rojo','Blanco','Blanco','Rojo']
+    assert first_population == ['Rojo','Blanco','Azul','Azul']
+   
 
 # =========================================================================== #
