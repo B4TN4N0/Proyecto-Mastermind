f7e1c2e4c332f890a1cd6c00354ce3fe554bcb50
joaquin240606fer-collab <joaquin240606fer@gmail.com>
2025-12-10 10:59:45 +0100

    feat(Añadir nuevos casos test): :sparkles: Añadir nuevos casos test
    Añadimos el caso test que comprueb aque se usan los colores validos para la formacion del genoma

--- Tests/Test_mastermind_game.py
+++ Tests/Test_mastermind_game.py
@@ -5,11 +5,11 @@ import pytest
 
 
 # ========================================================================== #
-# ==================== TESTS PRINCIPALES CON MASTERMIND ==================== #
+# ==================== TESTS GENERALES MASTERMIND ========================== #
 # ========================================================================== #
 
 
-
+# ================ TESTS DE GENERACION DE POBLACION INICIAL ================ #
 
 # ESTE TEST COMPROBARÁ QUE LA POBLACIÓN INICIAL TIENE EL TAMAÑO CORRECTO
 
@@ -24,9 +24,20 @@ def test_tamaño_poblacion_inicial(first_population):
 # ESTE TEST COMPROBARÁ QUE CADA INDIVIDUO DE LA POBLACIÓN INICIAL TIENE EL TAMAÑO CORRECTO
 
 @pytest.mark.people
-def test_tamaño_poblacion_inicial(first_population):
+def test_tamaño_genoma_poblacion_inicial(first_population):
     for individual in first_population:
         assert len(individual) == 4
 
 # ------------------------------------------------------------------ #
 
+# Este test comprueba que los colores en la población inicial son válidos
+
+@pytest.mark.people
+def test_colores_validos_poblacion_inicial(first_population,color_genes):
+    for gen in first_population:
+        for value in gen:
+            assert value in color_genes
+
+# ------------------------------------------------------------------ #
+
+# =========================================================================== #
