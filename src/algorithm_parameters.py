73eeb31d2c2f45e261d28c5cd1d67c53647e4cb6
joaquin240606fer-collab <joaquin240606fer@gmail.com>
2025-12-11 08:31:33 +0100

    feat(Añadir parametros): :sparkles: Añadri parametros para algoritmo genetico
    Añadir parametros generales de algoritmo genetico

---
+++ src/algorithm_parameters.py
@@ -0,0 +1,7 @@
+# Parámetros del Mastermind
+COLOR_GENES = ['Red', 'Green', 'Blue', 'Purple', 'Yellow', 'White', 'Pink', 'Orange']
+GENOME_LENGTH = 4
+
+# Parámetros del Algoritmo Genético
+POPULATION_SIZE = 100
+MUTATION_RATE = 0.05
