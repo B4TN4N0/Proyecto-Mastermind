c35f7ed5de662dfef35a2e4a82cfdb0b066759ae
joaq240606fer-collab <joaq240606fer@gmail.com>
2026-01-06 20:30:05 +0100

    feat(Añadir modulo de offspring): :sparkles: Añadir modulo de creacion de offspring
    crear mdulo de reproduccion de parents y de nueva generacion

---
+++ src/Reproduce_offspring.py
@@ -0,0 +1,24 @@
+import random
+
+def reproduce_offspring(selected_parents):
+    offspring_population = []
+    
+    # Recorremos la lista de dos en dos (paso de 2)
+    for i in range(0, len(selected_parents), 2):
+        # Aseguramos que haya un par disponible
+        if i + 1 < len(selected_parents):
+            parent1 = selected_parents[i]
+            parent2 = selected_parents[i+1]
+            
+            # Punto de cruce (Single Point Crossover)
+            cut_point = random.randint(1, len(parent1) - 1)
+            
+            # Creación de los dos hijos
+            offspring1 = parent1[:cut_point] + parent2[cut_point:]
+            offspring2 = parent2[:cut_point] + parent1[cut_point:]
+            
+            # Los añadimos a la nueva lista
+            offspring_population.append(offspring1)
+            offspring_population.append(offspring2)
+            
+    return offspring_population
